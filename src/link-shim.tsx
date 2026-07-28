7d53305ff06f1d4ecdd43d87bb857585d981c834
Nicole Arellano <nicolearelguz11@gmail.com>
2026-07-28 00:20:53 -0600

    Delete src directory

--- src/link-shim.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Link(props: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
-  return <a {...props} />;
-}
--- src/vph.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "../../app/globals.css";
-import VphPage from "../../app/vph/page";
-
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode><VphPage /></React.StrictMode>
-);
